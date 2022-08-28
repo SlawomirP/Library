0b4ce052ad2f301b1136fe2c0acd12462a30aeab
slawo <slawekpettke@gmail.com>
2022-08-28 22:21:02 +0200

    library

---
+++ src/main/java/MailService.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class MailService {
+
+    public void sendMail(final String address, final String topis, final String content, final List<Object> attachments){
+        System.out.println("Send mail!");
+    }
+}
